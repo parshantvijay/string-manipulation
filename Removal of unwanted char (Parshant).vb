7fca27d3aadaeb698db88934d84fc23c6d799cf3
Parshant Vijay <55455363+parshantvijay@users.noreply.github.com>
2019-10-06 22:18:31 +0500

    Add files via upload

---
+++ Removal of unwanted char (Parshant).vb
@@ -0,0 +1,33 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str1 As String
+        Dim str2 As String
+        Dim mychar As String
+        Dim nextchar As String
+        Dim counter As Integer
+
+        Str1 = ""
+        str2 = ""
+        mychar = ""
+        nextchar = ""
+        counter = 0
+
+        Console.Write("Enter Text = ")
+        Str1 = Console.ReadLine
+        Console.Write("Enter Character To Remove From The Text = ")
+        mychar = Console.ReadLine
+
+        For counter = 1 To Len(Str1)
+            nextchar = Mid(Str1, counter, 1)
+            If nextchar <> mychar Then
+                str2 = str2 & nextchar
+            End If
+        Next
+        Console.WriteLine("Text After Removal of Unwanted Character = " & str2)
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
